af8baf5d0b352a8e264fa7e484e1fa8c52264cb5
Adivyes <Adivyes@gmail.com>
2021-01-25 22:33:41 +0200

    init

---
+++ my-app/src/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css'
+
+export default function Home() {
+    let text = "this is home"
+    return (
+        <div class = "home">
+            <h1 id="hader">{text}</h1>
+            <h1 id="titel">hello world</h1>
+        </div>
+    )
+}
