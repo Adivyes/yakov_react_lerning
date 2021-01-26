8b1984c85f69a0a758b0d86bbbf766a951904531
Adivyes <Adivyes@gmail.com>
2021-01-26 13:29:05 +0200

    init

--- my-app/src/Home.jsx
+++ my-app/src/Home.jsx
@@ -2,10 +2,11 @@ import './Home.css'
 
 export default function Home() {
     let text = "this is home"
+    let titleId = "titel"
     return (
         <div class = "home">
             <h1 id="hader">{text}</h1>
-            <h1 id="titel">hello world</h1>
+            <h2 id={titleId}>hello world</h2>
         </div>
     )
 }
